698ea867eeaac4bf8d8d77a378555269032309cb
darusca <dario@webnanogy.co.za>
2017-07-18 09:07:56 +0200

    ~ extend PHPUnit_Framework

--- tests/unit/controllers/XmlOutputControllerTest.php
+++ tests/unit/controllers/XmlOutputControllerTest.php
@@ -4,7 +4,7 @@ namespace Tests\Unit;
 
 use Tests\TestCase;
 
-class XmlOutputControllerTest extends TestCase
+class XmlOutputControllerTest extends \PHPUnit_Framework_TestCase
 {
     public function tearDown()
     {
